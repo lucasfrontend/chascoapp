0fcc7fd9c67094f9664fd175354fd6ffd0c5aebb
lucasfrontend <lucasmartinezguardavidas@gmail.com>
2022-12-05 03:21:08 -0300

    sin pie

--- src/graficos/PilotsChart.jsx
+++ src/graficos/PilotsChart.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Pie } from 'react-chartjs-2';
+//import { Pie } from 'react-chartjs-2';
 /*
 import {Chart as ChartJS} from 'chart.js/auto'
 */
