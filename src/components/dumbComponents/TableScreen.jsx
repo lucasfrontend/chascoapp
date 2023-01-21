8880083c5638210819dd74abff4fb6f6f5adaf62
Lucas <61996547+lucasfrontend@users.noreply.github.com>
2023-01-21 10:39:21 -0300

    nueva tabla

--- src/components/dumbComponents/TableScreen.jsx
+++ src/components/dumbComponents/TableScreen.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 import ReactScrollableFeed from 'react-scrollable-feed'
 import Footer from "./Footer";
-import pilotito from '../../assets/pilots/pilotito.png'
-import tano from '../../assets/pilots/tano.png'
-import mati from '../../assets/pilots/mati.jpg'
-import ferl from '../../assets/pilots/ferl.png'
-import segundo from '../../assets/pilots/segundo.png'
-import martin from '../../assets/pilots/martin.png'
+import pink from '../../assets/aviones/pink.png'
+import green from '../../assets/aviones/green.png'
+import blue from '../../assets/aviones/blue.png'
+import cessna1 from '../../assets/aviones/cessna1.jpg'
+import cessna2 from '../../assets/aviones/cessna2.jpg'
+//import Spinner from "../spinner";
+import "./arrow.css"
+import "./table.css"
 
-const TableScreen = () => {
+const TableScreen2 = () => {
     const [tandas, setTandas] = useState(() => {
         const saveTandas = window.localStorage.getItem('tandasData');
         if(saveTandas) {
@@ -17,155 +19,137 @@ const TableScreen = () => {
             return []
         }
     })
-    
+
+    //const [loading, setLoading] = useState(false);
+    /*
+    const showSpinner = () => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+            navigate('/')
+        }, 4000);
+    }*/
+
     useEffect(() => {
         window.addEventListener('storage', () => {
           const tandas = JSON.parse(localStorage.getItem('tandasData'))
+          //showSpinner();
           setTandas(tandas);
         })
       }, []);
 
+
     return <>
-        <div className=""></div>
-        <div className="p-8" >
-            <div>
-                <h2 className="text-2xl font-semibold"></h2>
-                <div className=""></div>
-            </div>
-            <div className="w-full inline-block rounded-lg overflow-hidden ">
-                <table className="leading-normal w-full">
-                    <thead className="">
-                    <div className="bg-black ">
-                        <tr className="w-full flex justify-between p-4">
-                            <th className="text-3xl px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                                Nro
-                            </th>
-                            <th className="text-3xl px-5 py-3 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                                Paracas
-                            </th>
-                            <th className="text-3xl px-5 py-3 border-gray-200 bg-gray-100">
-                                Avión
-                            </th>
-                        </tr>
-                        </div >
-                    </thead>
-                    <tbody className="overflow-y-scroll" style={{
-                        height: '80vh',
-                    }}>
-                        <ReactScrollableFeed className="">
-                        {
-                            tandas.length === 0 ? <td className="min-w-full flex justify-center px-5 py-5 bg-white">Aun no hay tandas</td>
-                            : tandas.sort((a, b) => a.number_tanda - b.number_tanda)
-                            .map((tanda, index) => {
-                                return <>
-                                <tr className="min-w-full flex justify-between bg-white shadow-md border border-slate-300">
-                                    <td className="px-5 py-5 items-center text-center">
-                                        <span
-                                        className="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight"
-                                        >
-                                        <span
-                                            aria-hidden
-                                            className="absolute inset-0 bg-red-200 opacity-50 rounded-full"
-                                        ></span>
-                                        <span className="relative text-5xl text-blue">{ tanda.number_tanda }</span>
-                                        </span>
-                                    </td>
-                                    <td className="px-5 py-5 items-center text-center">
-                                        <p className="text-3xl text-gray-900 whitespace-no-wrap font-extrabold">{ tanda.paraca_1.charAt(0).toUpperCase() + tanda.paraca_1.slice(1)}</p>
-                                    </td>
-                                    <td className="px-  py-5 items-center text-center">
-                                        <p className="text-3xl text-gray-900 whitespace-no-wrap font-extrabold ">{ tanda.paraca_2.charAt(0).toUpperCase() + tanda.paraca_2.slice(1)}</p>
-                                    </td>
-                                    <td className="px-5 py-5">
-                                        <p className="text-3xl text-gray-900 whitespace-no-wrap font-extrabold">{ tanda.paraca_3.charAt(0).toUpperCase() + tanda.paraca_3.slice(1)}</p>
-                                    </td>
-                                    <td className="px-5 py-5">
-                                        <p className="text-3xl text-gray-900 whitespace-no-wrap font-extrabold">{ tanda.paraca_4.charAt(0).toUpperCase() + tanda.paraca_4.slice(1)}</p>
-                                    </td>
-                                    <td className="px-5 py-5">
-                                        <p className="text-3xl text-gray-900 whitespace-no-wrap font-extrabold">{ tanda.time}</p>
-                                    </td>
-                                    <td className="px-5 py-5 w-45">
-                                        <div className="flex">
-                                            {
-                                                tanda.pilot.toUpperCase() === 'PILOTITO' ? (
-                                                    <div className="flex-shrink-0 w-24 h-24">
-                                                        <img
-                                                        className="w-full h-full rounded-full"
-                                                        src={pilotito}
-                                                        alt=""
-                                                        />
-                                                    </div>
-                                                ) : ( 
-                                                    (tanda.pilot.toUpperCase() === 'TANO') ? (
-                                                        <div className="flex-shrink-0 w-24 h-24">
-                                                        <img
-                                                        className="w-full h-full rounded-full"
-                                                        src={tano}
-                                                        alt=""
-                                                        />
-                                                    </div>
-                                                    ) : ( 
-                                                        (tanda.pilot.toUpperCase() === 'FER LOPEZ') ? (
-                                                            <div className="flex-shrink-0 w-24 h-24">
-                                                            <img
-                                                            className="w-full h-full rounded-full"
-                                                            src={ferl}
-                                                            alt=""
-                                                            />
-                                                        </div>
-                                                        ) : (
-                                                            (tanda.pilot.toUpperCase() === 'MATI') ? (
-                                                                <div className="flex-shrink-0 w-24 h-24">
-                                                                <img
-                                                                className="w-full h-full rounded-full"
-                                                                src={mati}
-                                                                alt=""
-                                                                />
-                                                            </div>
-                                                            ) : (tanda.pilot.toUpperCase() === 'SEGUNDO') ? (
-                                                                <div className="flex-shrink-0 w-24 h-24">
-                                                                <img
-                                                                className="w-full h-full rounded-full"
-                                                                src={segundo}
-                                                                alt=""
-                                                                />
-                                                            </div>
+    {/* loading ? <Spinner/> : ''*/}
+        <div className="min-h-screen bg-gray-500 bg-login-screen backdrop-blur" >
+            <div className="p-4">
+                <div className="rounded-lg shadow-lg overflow-hidden">
+                    <table className="leading-normal w-full" id="table">
+                        <thead className="bg-blue text-white shadow-lg">
+                            <div className="">
+                                <tr className="">
+                                    <th className="px-4 py-3 text-white text-2xl">
+                                        Nro
+                                    </th>
+                                    <th className="w-1/4 px-5 py-3 text-left font-semibold text-2xl">Slot I
+                                    
+                                    </th>
+                                    <th className="w-1/4 px-5 py-3 text-left font-semibold text-2xl ">Slot II
+                                        
+                                    </th>
+                                    <th className="w-1/4 px-5 py-3 text-left font-semibold text-2xl ">Slot III
+                                    
+                                    </th>
+                                    <th className="w-1/4 px-5 py-3 text-left font-semibold text-2xl ">Slot IV
+                                        
+                                    </th>
+                                    <th className="w-1/4 px-5 py-3 font-semibold flex-col items-center text-2xl "></th>
+                                    <th className="w-1/4 px-5 py-3 font-semibold flex-col items-center text-2xl "></th>
+                                    <th className="w-1/4 px-5 py-3 font-semibold flex-col items-center text-2xl "></th>
+                                    <th className="w-1/4 px-5 py-3 font-semibold flex-col items-center text-2xl ">Avión</th>
+                                </tr>
+                            </div >
+                        </thead>
+                        <tbody className="" style={{
+                                height: '85vh'
+                            }}>
+                            <ReactScrollableFeed className="backdrop-blur">
+                                {
+                                    tandas.length === 0 ? <td className="flex justify-center text-2xl p-8 text-white">Aún  no hay tandas</td>
+                                    : tandas.sort((a, b) => a.number_tanda - b.number_tanda)
+                                    .map((tanda, index) => {
+                                        return <tr key={index} className="hover:bg-gray-100">
+                                                    <td className="px-4 py-3 text-white text-3xl"> 
+                                                        { tanda.number_tanda }
+                                                    </td>
+                                                    <td className="w-1/4 px-5 py-3 text-left font-semibold text-white text-3xl">
+                                                        { tanda.paraca_1.charAt(0).toUpperCase() + tanda.paraca_1.slice(1)}
+                                                    </td>
+                                                    <td className="w-1/4 px-5 py-3 text-left font-semibold text-white text-3xl">
+                                                        {tanda.paraca_2.charAt(0).toUpperCase() + tanda.paraca_2.slice(1)}
+                                                    </td>
+                                                    <td className="w-1/4 px-5 py-3 text-left font-semibold text-white text-3xl">
+                                                        {tanda.paraca_3.charAt(0).toUpperCase() + tanda.paraca_3.slice(1)}
+                                                    </td>
+                                                    <td className="w-1/4 px-5 py-3 text-left font-semibold text-white text-3xl">
+                                                        {tanda.paraca_4.charAt(0).toUpperCase() + tanda.paraca_4.slice(1)}
+                                                    </td>
+
+                                                    <div className="p-4">
+                                                        <td className="font-semibold text-white text-2xl pr-5">
+                                                            {tanda.time}
+                                                        </td>
+                                                        <td className="text-2xl text-white font-semibold pr-5">{tanda.altitude.charAt(0).toUpperCase() + tanda.altitude.slice(1)}</td>
+                                                        <td className="text-2xl text-white font-semibold pr-5">{tanda.pilot.charAt(0).toUpperCase() + tanda.pilot.slice(1)}</td>
+
+                                                        <td className="text-2xl text-white font-semibold pr-5">
+                                                        {
+                                                            tanda.avion.toUpperCase() === 'GYC' ? (
+                                                                <div className="text-2xl text-white">GYC</div>
+
                                                             ) : (
-                                                                <div className="flex-shrink-0 w-24 h-24">
-                                                                <img
-                                                                className="w-full h-full rounded-full"
-                                                                src={martin}
-                                                                alt=""
-                                                                />
-                                                            </div>
-                                                            ) 
-                                                        )
-                                                    
-                                                    )
-                                                
-                                                )
-                                            }
-                                            <div className="ml-6">
-                                                <p className="text-gray-900 whitespace-no-wrap text-4xl">
-                                                { tanda.avion }
-                                                </p>
-                                                <p className="text-gray-600 whitespace-no-wrap text-4xl">{ tanda.altitude }</p>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr> 
-                            </>
-                            })                            
-                        }
-                        </ReactScrollableFeed >
-                    </tbody>
-                </table>
+                                                                    tanda.avion.toUpperCase() === 'GRI' ? (
+                                                                        <div className="">GRI</div>
+                                                                    ) : (
+                                                                        tanda.avion.toUpperCase() === 'GSD' ? (
+                                                                            <div className="">GSD</div>
+                                                                        ) : (
+                                                                            ''
+                                                                        )
+                                                                    )
+                                                                )
+                                                        }
+                                                        </td>
+                                                    </div>
+                                                    <td className="font-semibold pr-5">
+                                                        {
+                                                            tanda.in_flight === true 
+                                                            ? 
+                                                                (
+                                                                    <div class="upload-btn-container">
+                                                                        <svg class="upload-arrow" xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 16 16" version="1.1" >
+                                                                        <path id="upload-arrow" d="M6 16l4 0 0-8 6 0 -8-8 -8 8 6 0 0 8Z" fill="#FF0000" />
+                                                                        </svg>
+                                                                    </div>
+                                                                ) 
+                                                            : 
+                                                                ''
+                                                        }
+                                                    </td>
+                                            </tr>
+                                        }
+                                    )
+                                }
+                            </ReactScrollableFeed >
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
         <Footer />
+
     </>
 
 }
 
-export default TableScreen
+export default TableScreen2
